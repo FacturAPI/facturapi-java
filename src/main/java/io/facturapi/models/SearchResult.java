597f49d96504d961004a3292e57c72b10d486ef1
javorosas <javier.rosasb@gmail.com>
2026-09-09 22:13:20 +0200

    chore: drop unused JsonProperty import in SearchResult

--- src/main/java/io/facturapi/models/SearchResult.java
+++ src/main/java/io/facturapi/models/SearchResult.java
@@ -2,7 +2,6 @@ package io.facturapi.models;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.List;
 
